1cc3a8928ee3df0c1bd80da5570594160805056f
Reinier Hernández <sasuke.reinier@gmail.com>
2022-02-21 12:15:55 -0500

    feat: add stats page

---
+++ pages/stats.jsx
@@ -0,0 +1,11 @@
+import BaseLayout from "../layouts/BaseLayout";
+
+const Stats = () => {
+  return (
+    <BaseLayout>
+      <h1>Estadísticas</h1>
+    </BaseLayout>
+  );
+};
+
+export default Stats;
