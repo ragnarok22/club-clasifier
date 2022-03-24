016fab38827a08d47acb37a284b9c5bb19263e53
Reinier Hernández <sasuke.reinier@gmail.com>
2022-03-24 14:10:27 -0400

    chore: update variables names!

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 export const TOKEN = "token";
-export const access_token = process.env.STRAVA_ACCESS_TOKEN;
-export const client_id = process.env.STRAVA_CLIENT_ID;
-export const client_secret = process.env.STAVA_CLIENT_SECRET;
-export const redirect_uri = process.env.STRAVA_REDIRECT_URI;
+export const access_token = process.env.NEXT_PUBLIC_STRAVA_ACCESS_TOKEN;
+export const client_id = process.env.NEXT_PUBLIC_STRAVA_CLIENT_ID;
+export const client_secret = process.env.NEXT_PUBLIC_STAVA_CLIENT_SECRET;
+export const redirect_uri = process.env.NEXT_PUBLIC_STRAVA_REDIRECT_URI;
